a5d46a022bef0484ed4f88cbcd9c1135e6c8563d
DJANFAROU <hanifdjanfarou8@gmail.com>
2025-02-19 22:15:03 +0000

    add the production linkedin

--- core/src/app/api.service.ts
+++ core/src/app/api.service.ts
@@ -7,7 +7,8 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  urlApi: string = 'http://127.0.0.1:8000/api/load/'
+  // urlApi: string = 'http://127.0.0.1:8000/api/load/' // En production
+  urlApi:string = "https://hanifcode.pythonanywhere.com/user/" // En production
 
   private http = inject(HttpClient)
   constructor() { }
